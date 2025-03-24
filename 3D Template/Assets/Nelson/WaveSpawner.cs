f4583b689e759d8a217bcbd9cd040a5c30f9fe87
MUKILTEO\1630107 <814830@stu.asd16.org>
2025-03-24 09:54:03 -0700

    Current progress on the wave system. It isn't finished, and it will have to be editied to fit the game

--- 3D Template/Assets/Nelson/WaveSpawner.cs
+++ 3D Template/Assets/Nelson/WaveSpawner.cs
@@ -1,27 +1,39 @@
 using UnityEngine;
-
+using System.Collections;
 public class WaveSpawner : MonoBehaviour
 {
     [SerializeField] private float countdown;
 
+    [SerializeField] private GameObject spawnPoint;
+
+    public Wave[] waves;
+
+    private int currentWaveIndex = 0;
     private void Update()
     {
         countdown -= Time.deltaTime;
 
         if (countdown <= 0)
         {
-            SpawnWave();
+            countdown = waves[currentWaveIndex].timeToNextWave;
+            StartCoroutine(SpawnWave());
         }
     }
 
-    private void SpawnWave()
+    private IEnumerator SpawnWave()
     {
-
+        for (int i = 0; i < waves[currentWaveIndex].enemies.Length; i++)
+        {
+            Instantiate(waves[currentWaveIndex].enemies[i], spawnPoint.transform);
+            yield return new WaitForSeconds(waves[currentWaveIndex].timeToNextEnemy);
+        }
     }
 }
 
 [System.Serializable]
 public class Wave
 {
-
+    public Enemy[] enemies;
+    public float timeToNextEnemy;
+    public float timeToNextWave;
 }
