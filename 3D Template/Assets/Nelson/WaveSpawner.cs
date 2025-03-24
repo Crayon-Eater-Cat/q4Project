042b502528ab7d0a4a717c79ca42edd8c8daadf7
MUKILTEO\1630107 <814830@stu.asd16.org>
2025-03-24 09:12:31 -0700

    Progress so far

---
+++ 3D Template/Assets/Nelson/WaveSpawner.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] private float countdown;
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            SpawnWave();
+        }
+    }
+
+    private void SpawnWave()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class Wave
+{
+
+}
