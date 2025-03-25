1415479d5f9b343f743a647de23cd524611d4d38
1717479 <1717479@mukilteo.wednet.edu>
2025-03-25 10:09:53 -0700

    Made the menu buttons do stuff, working on credits scene thing

--- 3D Template/Assets/Delsin/Scripts/SceneManagement.cs
+++ 3D Template/Assets/Delsin/Scripts/SceneManagement.cs
@@ -1,8 +1,10 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    int count = 0;
     public void Play()
     {
         SceneManager.LoadScene("Game");
@@ -10,10 +12,6 @@ public class SceneManagement : MonoBehaviour
     public void Settings()
     {
         SceneManager.LoadScene("Settings");
-    }
-    public void Quit()
-    {
-        Application.Quit();
     }
     public void Tutorial()
     {
@@ -26,5 +24,52 @@ public class SceneManagement : MonoBehaviour
     public void Return()
     {
         SceneManager.LoadScene("MenuMain");
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("Quit");
+    }
+    public void ExtraQuit()
+    {
+        SceneManager.LoadScene("QuitExtra");
+    }
+    public void Fine()
+    {
+        Application.Quit();
+    }
+    public void No()
+    {
+        SceneManager.LoadScene("MenuMain");
+    }
+    public void previous()
+    {
+        count--;
+    }
+    public void next()
+    {
+        count++;
+    }
+    public void CreditScreen()
+    {
+        if (count == 0)
+        {
+            count = 1;
+        }
+        if (count == 1)
+        {
+            SceneManager.LoadScene("Abigail");
+        }
+        if (count == 2)
+        {
+            SceneManager.LoadScene("Delsin");
+        }
+        if (count == 3)
+        {
+            SceneManager.LoadScene("Thomas");
+        }
+        if (count == 4)
+        {
+            count = 1;
+        }
     }
 }
