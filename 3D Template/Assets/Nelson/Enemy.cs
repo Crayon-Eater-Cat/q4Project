061c4fc79f54aeb34a741ca450c7144b67fb5d86
MUKILTEO\1720760 <1720760@mukilteo.wednet.edu>
2025-04-24 09:30:03 -0700

    Enemies attack and kill and can be killed.

--- 3D Template/Assets/Nelson/Enemy.cs
+++ 3D Template/Assets/Nelson/Enemy.cs
@@ -16,9 +16,20 @@ public class Enemy : MonoBehaviour
     float attacktimer;
     Animator anim;
     float ishit=0;
+
+    public float initialhealth;
+    float health;
+
+    public SphereCollider sc;
+
+    float attackcollider;
+
+    float attacking;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        health = initialhealth;
         player = GameObject.FindWithTag ("Player").transform;
         rayorigin = transform.GetChild(2);
         anim = GetComponent<Animator>();
@@ -27,6 +38,10 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Debug.Log("enemy health: "+health);
+
+
         rayorigin.LookAt(player);
         RaycastHit hit;
         if (Physics.Raycast(transform.position,rayorigin.forward,out hit,detectiondist))
@@ -40,6 +55,9 @@ public class Enemy : MonoBehaviour
                 else
                 {
                     mode = "attack";
+
+                    transform.LookAt(player.position);
+                    transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
                 }
             }
         }
@@ -57,24 +75,36 @@ public class Enemy : MonoBehaviour
         }
         if (attacktimer<=0 && mode == "attack")
         {
-            if (ishit==0)
+            if (ishit==0&&health>0)
             {
-
                 anim.SetTrigger("attack");
                 attacktimer = 10;
+                attacking = 5;
             }
         }
         if (attacktimer>0 )
         {
             attacktimer -= 1;
         }
-        if (Vector3.Distance(transform.position,player.position)>=idledist)
+
+        if (attacking>0)
+        {
+            attacking -= 1;
+        }
+
+        sc.enabled = attacking > 0;
+
+        if (Vector3.Distance(transform.position,player.position)>=idledist&&health>0)
         {
             mode = "idle";
         }
         if (ishit>0)
         {
             ishit -= 1;
+        }
+        else
+        {
+            anim.SetBool("hit", false);
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -83,5 +113,26 @@ public class Enemy : MonoBehaviour
         {
             other.GetComponent<PlayerStats>().TakeDamge(damage);
         }
+        if (other.CompareTag("attack"))
+        {
+
+            Debug.Log("struck");
+            if (health>=0)
+            {
+                anim.SetBool("hit", true);
+                health -= 5;
+                ishit = 50;
+                mode = "death";
+                Invoke("SelfDestruct", 2f);
+            }
+            else
+            {
+                anim.SetTrigger("death");
+            }  
+        }
+    }
+    public void SelfDestruct()
+    {
+        Destroy(gameObject);
     }
 }
--- 3D Template/Assets/Thomas/avatarMovement.cs
+++ 3D Template/Assets/Thomas/avatarMovement.cs
@@ -26,17 +26,19 @@ public class avatarMovement : MonoBehaviour
     float x2;
     float y2;
 
-    int life;
-
     public HealthBar hb;
 
     public string enemyattacktag;
 
-    bool invincible;
+    bool invincible=false;
 
     float invintimer;
 
     public SphereCollider sc;
+
+    bool dead=false;
+
+    PlayerStats ps;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -44,31 +46,43 @@ public class avatarMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         campivot = transform.GetChild(2);
-        hb.SetSliderMax(100);
+
+        ps = GetComponent<PlayerStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(ps.GetHealth());
 
         sc.enabled = attack;
         //get axis:
-        if (!attack)
+        if (!dead)
         {
+            if (!attack)
+            {
 
-            x = Input.GetAxisRaw("Horizontal");
-            y = Input.GetAxisRaw("Vertical");
-        }
-        if (!mousecontrolled)
-        {
+                x = Input.GetAxisRaw("Horizontal");
+                y = Input.GetAxisRaw("Vertical");
+            }
+            if (!mousecontrolled)
+            {
 
-            x2 = Input.GetAxis("Horizontal2");
-            y2 = Input.GetAxis("Vertical2");
+                x2 = Input.GetAxis("Horizontal2");
+                y2 = Input.GetAxis("Vertical2");
+            }
+            else
+            {
+                x2 = Input.GetAxis("Mouse X");
+                y2 = Input.GetAxis("Mouse Y");
+            }
         }
         else
         {
-            x2 = Input.GetAxis("Mouse X");
-            y2 = Input.GetAxis("Mouse Y");
+            x = 0;
+            y = 0;
+            x2 = 0;
+            y2 = 0;
         }
 
         //animation:
@@ -121,7 +135,7 @@ public class avatarMovement : MonoBehaviour
         canjump = Physics.BoxCast(transform.position, new Vector3(0, 1, 0), -transform.up, Quaternion.Euler(Vector3.zero), 1);
         anim.SetBool("jumping", !canjump);
         //combat:
-        if (Input.GetKeyDown(attackkey)&&!attack&&canjump)
+        if (Input.GetKeyDown(attackkey)&&!attack&&canjump&&!dead)
         {
             attack = true;
             timer = 30;
@@ -162,16 +176,16 @@ public class avatarMovement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag==enemyattacktag&&!invincible)
+        if (other.gameObject.tag==enemyattacktag&&!invincible)
         {
             invincible = true;
-            life -= 3;
-            timer = invincibilityDuration;
-            hb.SetSlider(life);
+            ps.DecreaseHealth(3);
+            invintimer = invincibilityDuration;
             anim.SetTrigger("hit");
-            if (life<=0)
+            if (ps.GetHealth()<=0)
             {
-                anim.SetTrigger("death");
+                anim.SetTrigger("dead");
+                dead = true;
             }
         }
     }
