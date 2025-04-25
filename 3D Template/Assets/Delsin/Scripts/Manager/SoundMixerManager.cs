14c2293e1c03fd737813452f1ba9c3340ec2186b
1717479 <1717479@mukilteo.wednet.edu>
2025-04-25 10:18:38 -0700

    Working on slider plolish stuffs

--- 3D Template/Assets/Delsin/Scripts/Manager/SoundMixerManager.cs
+++ 3D Template/Assets/Delsin/Scripts/Manager/SoundMixerManager.cs
@@ -1,22 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
+    public float masterVolumeSlider {  get; set; }
+    public float inMusicVolumeSlider { get; set; }
+    public float mMusicVolumeSlider { get; set; }
+    public float masterLvl;
+    public float inMusicLvl;
+    public float mMusicLvl;
+    //public List<AudioMixer> AudioMixerList;
     public void SetMasterVolume(float level)
     {
-        audioMixer.SetFloat("masterVolume", Mathf.Log(level) * 20);
+        audioMixer.SetFloat("masterVolume", Mathf.Log(masterLvl) * 20);
+        masterVolumeSlider = PlayerPrefs.GetFloat("masterVolume", masterLvl);
     }
     public void SetGameMusicVolume(float level)
     {
-        audioMixer.SetFloat("musicGameVolume", Mathf.Log(level) * 20);
-
+        audioMixer.SetFloat("musicGameVolume", Mathf.Log(inMusicLvl) * 20);
+        inMusicVolumeSlider = PlayerPrefs.GetFloat("musicGameVolume", inMusicLvl);
     }
     public void SetMenuMusicVolume(float level)
     {
-        audioMixer.SetFloat("musicMenuVolume", Mathf.Log(level) * 20);
-
+        audioMixer.SetFloat("musicMenuVolume", Mathf.Log(mMusicLvl) * 20);
+        mMusicVolumeSlider = PlayerPrefs.GetFloat("musicMenuVolume", mMusicLvl);
     }
-
+    public void SetMasterVolumeSlider(float level)
+    {
+        PlayerPrefs.SetFloat(masterLvl, level);
+    }
 }
