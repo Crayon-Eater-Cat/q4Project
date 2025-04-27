2067674d163523d272027739dd88919db1eaa4c0
1717479 <1717479@mukilteo.wednet.edu>
2025-04-27 12:29:17 -0700

    Volume slider value saves now

--- 3D Template/Assets/Delsin/Scripts/Manager/SoundMixerManager.cs
+++ 3D Template/Assets/Delsin/Scripts/Manager/SoundMixerManager.cs
@@ -1,34 +1,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SoundMixerManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
-    public float masterVolumeSlider {  get; set; }
-    public float inMusicVolumeSlider { get; set; }
-    public float mMusicVolumeSlider { get; set; }
+
     public float masterLvl;
     public float inMusicLvl;
     public float mMusicLvl;
+
+    public Slider masterVolSlide;
+    public Slider inMusicVolSlide;
+    public Slider mMusicVolSlide;
+
     //public List<AudioMixer> AudioMixerList;
     public void SetMasterVolume(float level)
     {
+        masterLvl = level;
         audioMixer.SetFloat("masterVolume", Mathf.Log(masterLvl) * 20);
-        masterVolumeSlider = PlayerPrefs.GetFloat("masterVolume", masterLvl);
+        PlayerPrefs.SetFloat("masterVolume", level);
     }
     public void SetGameMusicVolume(float level)
     {
+        inMusicLvl = level;
         audioMixer.SetFloat("musicGameVolume", Mathf.Log(inMusicLvl) * 20);
-        inMusicVolumeSlider = PlayerPrefs.GetFloat("musicGameVolume", inMusicLvl);
+        PlayerPrefs.SetFloat("musicGameVolume", level);
     }
     public void SetMenuMusicVolume(float level)
     {
+        mMusicLvl = level;
         audioMixer.SetFloat("musicMenuVolume", Mathf.Log(mMusicLvl) * 20);
-        mMusicVolumeSlider = PlayerPrefs.GetFloat("musicMenuVolume", mMusicLvl);
+        PlayerPrefs.SetFloat("musicMenuVolume", level);
     }
-    public void SetMasterVolumeSlider(float level)
+    public void Start()
     {
-        PlayerPrefs.SetFloat(masterLvl, level);
+        masterVolSlide.value = PlayerPrefs.GetFloat("masterVolume", masterLvl);
+        inMusicVolSlide.value = PlayerPrefs.GetFloat("musicGameVolume", inMusicLvl);
+        mMusicVolSlide.value = PlayerPrefs.GetFloat("musicMenuVolume", mMusicLvl);
     }
 }
