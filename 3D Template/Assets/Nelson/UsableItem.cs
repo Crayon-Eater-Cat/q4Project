c90b3c248dc98074078611826d5283ab63e38780
MUKILTEO\1630107 <814830@stu.asd16.org>
2025-04-28 07:43:26 -0700

    YEs

--- 3D Template/Assets/Nelson/UsableItem.cs
+++ 3D Template/Assets/Nelson/UsableItem.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class UsableItem : Masks
 {
+    public bool IsUseable;
     public virtual void Use(PlayerStats playerstats, avatarMovement playermovement)
     {
 
