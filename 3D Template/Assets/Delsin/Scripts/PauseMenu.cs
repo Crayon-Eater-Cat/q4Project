dacb126b02fe2095663571f2c58db5b46cb39c7f
1717479 <1717479@mukilteo.wednet.edu>
2025-05-02 09:18:08 -0700

    Fixing issues

--- 3D Template/Assets/Delsin/Scripts/PauseMenu.cs
+++ 3D Template/Assets/Delsin/Scripts/PauseMenu.cs
@@ -6,11 +6,8 @@ public class PauseMenu : MonoBehaviour
     public KeyCode pause;
     private int toggle;
     public GameObject pauseMenu;
+    //public GameObject resumeButton;
     public void Start()
-    {
-        pauseMenu.SetActive(false);
-    }
-    public void Resume()
     {
         pauseMenu.SetActive(false);
     }
@@ -23,14 +20,21 @@ public class PauseMenu : MonoBehaviour
         }
         if (toggle == 1)
         {
-            Debug.Log("Paused");
+            Debug.Log("Paused : " + toggle);
             pauseMenu.SetActive(true);
         }
         if (toggle >= 2)
         {
-            Debug.Log("Resume");
+            Debug.Log("Resume : " + toggle);
             pauseMenu.SetActive(false);
             toggle = 0;
+            Debug.Log(toggle);
         }
+    }
+    public void Resume()
+    {
+        toggle++;
+        pauseMenu.SetActive(false);
+        Debug.Log("Resume");
     }
 }
