d6f554a9c5f7013c3f67d1f6e0b490b1dbfd507e
MUKILTEO\1630107 <814830@stu.asd16.org>
2025-05-05 09:03:31 -0700

    Push

--- 3D Template/Assets/Nelson/HealingItem.cs
+++ 3D Template/Assets/Nelson/HealingItem.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class HealingItem : MonoBehaviour
 {
     public float healAmount;
+    PlayerStats health;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && health.currentHealth > health.maxHealth)
         {
             other.GetComponent<PlayerStats>().Heal(healAmount);
             Destroy(gameObject);
